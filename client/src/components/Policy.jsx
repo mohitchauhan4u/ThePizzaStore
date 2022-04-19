223ca30547421d5ddd78f02d6a4b6f17d1f4b664
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-04-19 18:30:40 +0530

    created dummy policy page

--- client/src/components/Policy.jsx
+++ client/src/components/Policy.jsx
@@ -5,7 +5,79 @@ const Policy = () => {
   return (
     <>
       <Container style={{ marginTop: "50px" }}>
-        <h1>Terms</h1>
+        <h1>Terms and policy</h1>
+        <Row>
+          <Col md={10}>
+            <h5>Cupidatat cupidatat deserunt ipsum magna occaecat.</h5>
+            <p>
+              Nostrud elit ipsum quis adipisicing nulla adipisicing cupidatat
+              irure esse. Sint duis commodo excepteur ad ullamco culpa ex dolor
+              velit excepteur Lorem sit excepteur. Pariatur veniam ipsum esse
+              aliquip aliquip fugiat elit et tempor reprehenderit velit
+              cupidatat. Veniam proident id velit tempor eiusmod cillum quis
+              dolor. Eiusmod ad et occaecat duis in sunt aliquip adipisicing
+              velit aliquip fugiat proident deserunt esse. Excepteur voluptate
+              duis et enim cupidatat eiusmod excepteur esse enim sunt occaecat
+              ullamco.
+            </p>
+          </Col>
+          <Col md={10}>
+            <h5>Cupidatat cupidatat deserunt ipsum magna occaecat.</h5>
+            <p>
+              Nostrud elit ipsum quis adipisicing nulla adipisicing cupidatat
+              irure esse. Sint duis commodo excepteur ad ullamco culpa ex dolor
+              velit excepteur Lorem sit excepteur. Pariatur veniam ipsum esse
+              aliquip aliquip fugiat elit et tempor reprehenderit velit
+              cupidatat. Veniam proident id velit tempor eiusmod cillum quis
+              dolor. Eiusmod ad et occaecat duis in sunt aliquip adipisicing
+              velit aliquip fugiat proident deserunt esse. Excepteur voluptate
+              duis et enim cupidatat eiusmod excepteur esse enim sunt occaecat
+              ullamco.
+            </p>
+          </Col>
+          <Col md={10}>
+            <h5>Cupidatat cupidatat deserunt ipsum magna occaecat.</h5>
+            <p>
+              Nostrud elit ipsum quis adipisicing nulla adipisicing cupidatat
+              irure esse. Sint duis commodo excepteur ad ullamco culpa ex dolor
+              velit excepteur Lorem sit excepteur. Pariatur veniam ipsum esse
+              aliquip aliquip fugiat elit et tempor reprehenderit velit
+              cupidatat. Veniam proident id velit tempor eiusmod cillum quis
+              dolor. Eiusmod ad et occaecat duis in sunt aliquip adipisicing
+              velit aliquip fugiat proident deserunt esse. Excepteur voluptate
+              duis et enim cupidatat eiusmod excepteur esse enim sunt occaecat
+              ullamco.
+            </p>
+          </Col>
+          <Col md={10}>
+            <h5>Cupidatat cupidatat deserunt ipsum magna occaecat.</h5>
+            <p>
+              Nostrud elit ipsum quis adipisicing nulla adipisicing cupidatat
+              irure esse. Sint duis commodo excepteur ad ullamco culpa ex dolor
+              velit excepteur Lorem sit excepteur. Pariatur veniam ipsum esse
+              aliquip aliquip fugiat elit et tempor reprehenderit velit
+              cupidatat. Veniam proident id velit tempor eiusmod cillum quis
+              dolor. Eiusmod ad et occaecat duis in sunt aliquip adipisicing
+              velit aliquip fugiat proident deserunt esse. Excepteur voluptate
+              duis et enim cupidatat eiusmod excepteur esse enim sunt occaecat
+              ullamco.
+            </p>
+          </Col>
+          <Col md={10}>
+            <h5>Cupidatat cupidatat deserunt ipsum magna occaecat.</h5>
+            <p>
+              Nostrud elit ipsum quis adipisicing nulla adipisicing cupidatat
+              irure esse. Sint duis commodo excepteur ad ullamco culpa ex dolor
+              velit excepteur Lorem sit excepteur. Pariatur veniam ipsum esse
+              aliquip aliquip fugiat elit et tempor reprehenderit velit
+              cupidatat. Veniam proident id velit tempor eiusmod cillum quis
+              dolor. Eiusmod ad et occaecat duis in sunt aliquip adipisicing
+              velit aliquip fugiat proident deserunt esse. Excepteur voluptate
+              duis et enim cupidatat eiusmod excepteur esse enim sunt occaecat
+              ullamco.
+            </p>
+          </Col>
+        </Row>
       </Container>
     </>
   );
