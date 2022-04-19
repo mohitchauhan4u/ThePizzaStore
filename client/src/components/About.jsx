7b355e41418ae337830a27c13c282aba4d4fe492
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-04-19 10:37:08 +0530

    creating dummy about page

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -2,9 +2,90 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 const About = () => {
   return (
-    <>
-      <Container></Container>
-    </>
+    <Container>
+      <h1>Who we are?</h1>
+      <p>
+        Occaecat aliquip eu ex quis. Laborum eiusmod non nisi enim ullamco amet
+        ullamco. Ad do aliquip duis ullamco. Ex elit in ex ipsum enim nulla
+        cillum reprehenderit anim. Qui ullamco non officia sit veniam amet
+        eiusmod cillum eiusmod dolor.In dolor culpa Lorem culpa officia magna
+        qui sint fugiat dolor. Veniam esse qui non fugiat cupidatat. Deserunt
+        ipsum sunt commodo enim. Ad anim excepteur adipisicing minim ut dolor
+        reprehenderit duis velit nostrud adipisicing.
+      </p>
+      <h1>Our speciality</h1>
+      <Row>
+        <Col md={6}>
+          <p>
+            Ex ipsum consectetur voluptate adipisicing anim. Cillum aliquip esse
+            aliquip excepteur. Do veniam fugiat sunt velit nostrud officia
+            commodo minim labore do laborum in. Laborum cupidatat ad duis
+            occaecat ad dolor proident.Proident sunt quis dolore fugiat culpa do
+            laborum proident dolore id. Eu cupidatat commodo sint aliqua laboris
+            magna culpa dolore Lorem. Cillum labore ad sint occaecat ullamco
+            laborum do quis ipsum qui pariatur cillum ut sit. Et laborum dolor
+            occaecat velit sint occaecat magna laborum aliquip ex aute duis.
+            Culpa veniam anim incididunt esse pariatur eu.
+          </p>
+        </Col>
+        <Col md={6}>
+          <p>
+            Ex ipsum consectetur voluptate adipisicing anim. Cillum aliquip esse
+            aliquip excepteur. Do veniam fugiat sunt velit nostrud officia
+            commodo minim labore do laborum in. Laborum cupidatat ad duis
+            occaecat ad dolor proident.Proident sunt quis dolore fugiat culpa do
+            laborum proident dolore id. Eu cupidatat commodo sint aliqua laboris
+            magna culpa dolore Lorem. Cillum labore ad sint occaecat ullamco
+            laborum do quis ipsum qui pariatur cillum ut sit. Et laborum dolor
+            occaecat velit sint occaecat magna laborum aliquip ex aute duis.
+            Culpa veniam anim incididunt esse pariatur eu.
+          </p>
+        </Col>
+      </Row>
+      <Row>
+        <h1>Our Chief</h1>
+        <Col md={3}>
+          Irure enim aute non velit eiusmod adipisicing anim eu in incididunt.
+          Consectetur duis voluptate duis voluptate ipsum commodo est ea Lorem
+          pariatur irure deserunt commodo. Laborum quis ad exercitation
+          consectetur ut tempor consequat est tempor. Veniam deserunt ipsum
+          dolor laboris elit occaecat excepteur laboris cillum nisi et. Mollit
+          sit ullamco amet aliqua labore amet exercitation cupidatat veniam eu
+          proident. Exercitation non proident pariatur sunt elit ea. Et sunt sit
+          nisi culpa.
+        </Col>
+        <Col md={3}>
+          Irure enim aute non velit eiusmod adipisicing anim eu in incididunt.
+          Consectetur duis voluptate duis voluptate ipsum commodo est ea Lorem
+          pariatur irure deserunt commodo. Laborum quis ad exercitation
+          consectetur ut tempor consequat est tempor. Veniam deserunt ipsum
+          dolor laboris elit occaecat excepteur laboris cillum nisi et. Mollit
+          sit ullamco amet aliqua labore amet exercitation cupidatat veniam eu
+          proident. Exercitation non proident pariatur sunt elit ea. Et sunt sit
+          nisi culpa.
+        </Col>
+        <Col md={3}>
+          Irure enim aute non velit eiusmod adipisicing anim eu in incididunt.
+          Consectetur duis voluptate duis voluptate ipsum commodo est ea Lorem
+          pariatur irure deserunt commodo. Laborum quis ad exercitation
+          consectetur ut tempor consequat est tempor. Veniam deserunt ipsum
+          dolor laboris elit occaecat excepteur laboris cillum nisi et. Mollit
+          sit ullamco amet aliqua labore amet exercitation cupidatat veniam eu
+          proident. Exercitation non proident pariatur sunt elit ea. Et sunt sit
+          nisi culpa.
+        </Col>
+        <Col md={3}>
+          Irure enim aute non velit eiusmod adipisicing anim eu in incididunt.
+          Consectetur duis voluptate duis voluptate ipsum commodo est ea Lorem
+          pariatur irure deserunt commodo. Laborum quis ad exercitation
+          consectetur ut tempor consequat est tempor. Veniam deserunt ipsum
+          dolor laboris elit occaecat excepteur laboris cillum nisi et. Mollit
+          sit ullamco amet aliqua labore amet exercitation cupidatat veniam eu
+          proident. Exercitation non proident pariatur sunt elit ea. Et sunt sit
+          nisi culpa.
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
