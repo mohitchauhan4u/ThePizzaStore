2c5dbc295eea8a808aedb567305b15000f7ef5fe
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-04-20 22:02:51 +0530

    creating pizaa.jsx

--- client/src/components/Pizza.jsx
+++ client/src/components/Pizza.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { Card, Button, Row, Col } from "react-bootstrap";
 
-const Pizza = () => {
-  return <div>Pizzas</div>;
+const Pizza = ({ pizza }) => {
+  return (
+    <>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src={pizza.image} />
+        <Card.Body>
+          <Card.Title>{pizza.name}</Card.Title>
+          <Card.Text>
+            <Row>
+              <Col md={6}>
+                <h6>variant</h6>
+                <select>
+                  {pizza.varients.map((varient) => (
+                    <option value={varient}>{varient}</option>
+                  ))}
+                </select>
+              </Col>
+              <Col md={6}>
+                <h6>quantity</h6>
+                <select>
+                  {[...Array(10).keys()].map((v, i) => (
+                    <option value={i + 1}>{i + 1}</option>
+                  ))}
+                </select>
+              </Col>
+            </Row>
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+    </>
+  );
 };
 
 export default Pizza;
