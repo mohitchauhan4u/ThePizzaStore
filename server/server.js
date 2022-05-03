48c8c5d38a14ac373825c1ac9858f7b2d7aa9785
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-05-03 18:13:18 +0530

    conected mongo

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(morgan("dev"));
 
 //route
 app.get("/", (req, res) => {
-  res.send("<h1>Hello h node server ki taraf se</h1>");
+  res.send("<h1>Hello h node server ki taraf se bhai ko</h1>");
 });
 app.listen(8080, () => {
   console.log("server running on port 8080");
