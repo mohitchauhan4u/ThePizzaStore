bd76c2affce9c44492206a1aa65ef7795f5bdbca
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-05-05 10:51:07 +0530

    removed error

--- client/src/store.js
+++ client/src/store.js
@@ -1,4 +1,4 @@
-import { combineReducers, applyMiddleware, configureStore } from "redux";
+import { combineReducers, applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { getAllPizzaReducer } from "./reducers/pizzaReducers";
@@ -8,7 +8,7 @@ const rootReducer = combineReducers({
 });
 const initialState = {};
 const middleware = [thunk];
-const store = configureStore(
+const store = createStore(
   rootReducer,
   initialState,
   composeWithDevTools(applyMiddleware(...middleware))
