46b3af228bd8723ce4822157bea0dae9c85adcda
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-05-07 12:27:04 +0530

    created cartAction

--- client/src/actions/cartAction.js
+++ client/src/actions/cartAction.js
@@ -1,1 +1,12 @@
-export const addToCart = () => {};
+export const addToCart = (pizza, quantity, varient) => {
+  var cartItem = {
+    name: pizza.name,
+    _is: pizza._id,
+    image: pizza.image,
+    varient: varient,
+    quantity: quantity,
+    price: pizza.prices,
+    price: pizza.prices[0][varient] * quantity,
+  };
+  dispatchEvent({ type: "ADD_TO_CART", payload: cartItem });
+};
