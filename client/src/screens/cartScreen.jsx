2127c5833987e5cc79e5115859895da9dc821462
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-05-07 15:01:41 +0530

    added functionality to show name,varient in cart

--- client/src/screens/cartScreen.jsx
+++ client/src/screens/cartScreen.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import { useSelector } from "react-redux";
 const CartScreen = () => {
+  const cartState = useSelector((state) => state.cartReducer);
+  const cartItems = cartState.cartItems;
   return (
     <>
       <Container>
         <Row>
           <Col md={6}>
-            <h1>My Cart Items</h1>
+            <h1>My Cart</h1>
+            <Row>
+              {cartItems.map((item) => (
+                <>
+                  <Col md={7}>
+                    <h5>
+                      {item.name} [{item.varient}]
+                    </h5>
+                    <h6></h6>
+                  </Col>
+                  <Col md={5}></Col>
+                </>
+              ))}
+            </Row>
           </Col>
           <Col md={4}>
             <h1>Payment Info</h1>
