4402394af0ae17645c2fa0ea5ef1188ac2c22c8a
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-05-07 12:05:45 +0530

    removed error

--- client/src/actions/pizzaAction.js
+++ client/src/actions/pizzaAction.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const getAllPizzas = () => async (dispatch) => {
   dispatch({ type: "GET_PIZZAS_REQUEST" });
   try {
-    const res = await axios.get("/api/pizzas/getPizzas");
+    const res = await axios.get("/api/pizzas/getAllPizzas");
     console.log(res);
     dispatch({ type: "GET_PIZZAS_SUCCESS", payload: res.data });
   } catch (err) {
