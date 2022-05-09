57df194c9ab381ed83c6acbec0918560c84e40a2
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-05-09 16:50:13 +0530

    changed variable name

--- client/src/actions/pizzaAction.js
+++ client/src/actions/pizzaAction.js
@@ -3,9 +3,9 @@ import axios from "axios";
 export const getAllPizzas = () => async (dispatch) => {
   dispatch({ type: "GET_PIZZAS_REQUEST" });
   try {
-    const res = await axios.get("/api/pizzas/getAllPizzas");
-    console.log(res);
-    dispatch({ type: "GET_PIZZAS_SUCCESS", payload: res.data });
+    const response = await axios.get("/api/pizzas/getAllPizzas");
+    console.log(response.data);
+    dispatch({ type: "GET_PIZZAS_SUCCESS", payload: response.data });
   } catch (err) {
     dispatch({ type: "GET_PIZZAS_FAIL", payload: err });
   }
