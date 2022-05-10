abb8d6c19845a08d2fdaab88a593ab88eb673409
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-05-10 17:57:07 +0530

    Created Admin Screen

--- client/src/screens/AdminScreen.jsx
+++ client/src/screens/AdminScreen.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { Container, Row, Col, Button, ButtonGroup } from "react-bootstrap";
 
 const AdminScreen = () => {
-  return <h1>AdminScreen</h1>;
+  return (
+    <>
+      <>
+        <Container>
+          <Row>
+            <h1 className="text-center bg-dark text-light p-2">Admin Panel</h1>
+            <Col md={4}>
+              <ButtonGroup vertical style={{ minHeight: "200px" }}>
+                <Button>All Users</Button>
+                <Button>All Pizzas</Button>
+                <Button>Add new Pizza</Button>
+                <Button>All orders</Button>
+              </ButtonGroup>
+            </Col>
+          </Row>
+        </Container>
+      </>
+    </>
+  );
 };
 
 export default AdminScreen;
